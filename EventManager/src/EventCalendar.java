f461a11b13339f3096d6dd1b6155e389bb7c584e
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-09-23 14:49:26 -0400

    Update EventCalendar.java

--- EventManager/src/EventCalendar.java
+++ EventManager/src/EventCalendar.java
@@ -1,9 +1,21 @@
 public class EventCalendar {
+    //constants
+    private static final int NOT_FOUND = -1;
+    private static final int INITIAL_CAPACITY = 4;
+    private static final int CAPACITY_INCRMENT = 4;
+    
     private Event[] events;
     private int numEvents;
-    private int find(Event event){
 
+    private int find(Event event){
+    for(int i = 0; i < numEvents;i++){
+            if(events[i].equals(event)){
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
+
     private void grow(){
 
     }
