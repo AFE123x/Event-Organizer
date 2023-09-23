8fb5ca4696951d3cb0ae601e3a5e59f7657a6dd1
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-09-23 14:55:02 -0400

    Update EventCalendar.java
    Added two methods , find and grow . initialized 3 final constants.

--- EventManager/src/EventCalendar.java
+++ EventManager/src/EventCalendar.java
@@ -2,7 +2,7 @@ public class EventCalendar {
     //constants
     private static final int NOT_FOUND = -1;
     private static final int INITIAL_CAPACITY = 4;
-    private static final int CAPACITY_INCRMENT = 4;
+    private static final int CAPACITY_INCREMENT = 4;
     
     private Event[] events;
     private int numEvents;
@@ -17,7 +17,11 @@ public class EventCalendar {
     }
 
     private void grow(){
-
+    Event NewEvents[] = new Event[events.length + CAPACITY_INCREMENT];
+        for(int i = 0 ; i < events.length;i++){
+            NewEvents[i] = events[i];
+        }
+        events = NewEvents;
     }
     public boolean add(Event event){
 
