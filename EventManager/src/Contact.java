043094edf81bdfce1811650afe3cc1f24269f655
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-09-25 20:50:27 -0400

    Update Contact.java
    Completed Contact.java

--- EventManager/src/Contact.java
+++ EventManager/src/Contact.java
@@ -5,7 +5,58 @@ public class Contact {
         this.department = department;
         this.email = email;
     }
-    public boolean isValid(){
+    //getters and setters
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // is valid method
+    public boolean isValid(){
+        if(email == null || department == null){
+            return false;
+        }
+    
+        String mailEnd = "@rutgers.edu";
+        boolean isEmailValid = email.length() > mailEnd.length() && email.endsWith(mailEnd);
+        
+        boolean isDepartmentValid = false;
+        for(Department d: Department.values()){
+            if(department.equals(d)){
+                isDepartmentValid = true;
+                break; 
+            }
+        }
+        return isEmailValid && isDepartmentValid;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Contact{ department=" + department + ", email='" + email + "' }";
+    }
+    
+    // equals 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){ 
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Contact contact = (Contact) o;
+        return department == contact.department && email.equals(contact.email);
     }
 }
