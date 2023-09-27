4e73af365ef4904a6f90842967c7910c85d51314
AFE123x <121839885+AFE123x@users.noreply.github.com>
2023-09-27 08:46:26 -0400

    Update

--- EventManager/src/EventOrganizer.java
+++ EventManager/src/EventOrganizer.java
@@ -1,66 +1,68 @@
 import java.util.Scanner;
 
 public class EventOrganizer {
-        private Scanner scanner;
-        private String userinput;
-        private Boolean isRunning;
-        // private EventCalendar eventcalendar;
-        public EventOrganizer(){
-            this.scanner = new Scanner(System.in);
-            this.userinput = "";
-            this.isRunning = true;
-            // this.eventcalendar = new EventCalendar();
-        }
-        
-        public void run(){
-            while(this.isRunning){
-                userinput = scanner.nextLine();
-                if(userinput.equals("Q")){
-                    this.isRunning = false;
+    private Scanner scanner;
+    private String userinput;
+    private Boolean isRunning;
 
+    // private EventCalendar eventcalendar;
+    public EventOrganizer() {
+        this.scanner = new Scanner(System.in);
+        this.userinput = "";
+        this.isRunning = true;
+        // this.eventcalendar = new EventCalendar();
+    }
+
+    public void run() {
+        while (this.isRunning) {
+            userinput = scanner.nextLine();
+            if (userinput.equals("Q")) {
+                this.isRunning = false;
+
+            } else {
+                String[] parts = userinput.split("\\s+");
+                switch (parts[0]) {
+                    case "P":
+                        //function()
+                        break;
+                    case "OE":
+                        //function()
+                        break;
+                    case "PC":
+                        //function()
+                        break;
+                    case "PD":
+                        //function()
+                        break;
+                    case "A":
+                        addhelper(parts);
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
                 }
-                else{
-                    String[] parts = userinput.split("\\s+");
-                    switch(parts[0]){
-                        case "P":
-                            //function()
-                            break;
-                        case "OE":
-                            //function()
-                            break;
-                        case "PC":
-                            //function()
-                            break;
-                        case "PD":
-                            //function()
-                            break;
-                        case "A":
-                            addhelper(parts);
-                            break;
-                        default:
-                            System.out.println("Invalid option");
-                            break;
-                    }
-            } 
-        } 
-    } 
-    private void addhelper(String [] parts){
+            }
+        }
+    }
+
+    private void addhelper(String[] parts) {
         ////public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration)
         Date date = extractDate(parts[1]);
-        if(date == null){
+        if (date == null) {
             System.out.println("Date format incorrect");
             return;
         }
         Timeslot temp = getTimeSlot(parts[2]);
-        if(temp == null){
+        if (temp == null) {
             System.out.println("Invalid Timeslot");
             return;
         }
         System.out.println(temp.getHour() + "    " + date);
-        
+
     }
-    private Timeslot getTimeSlot(String mystring){
-        switch(mystring.toLowerCase()){
+
+    private Timeslot getTimeSlot(String mystring) {
+        switch (mystring.toLowerCase()) {
             case "morning":
                 return Timeslot.MORNING;
             case "evening":
@@ -72,16 +74,18 @@ public class EventOrganizer {
 
         }
     }
-    private Date extractDate(String myString){
+
+    private Date extractDate(String myString) {
         try {
-        String[] dateArray = dateString.split("/");
-        int day = Integer.parseInt(dateArray[0]);
-        int month = Integer.parseInt(dateArray[1]);
-        int year = Integer.parseInt(dateArray[2]);
-        return new Date(year, month, day);
-    } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
-        return null; // Returns null if the date string is invalid.
+            String[] dateArray = myString.split("/");
+            int day = Integer.parseInt(dateArray[0]);
+            int month = Integer.parseInt(dateArray[1]);
+            int year = Integer.parseInt(dateArray[2]);
+            return new Date(year, month, day);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            return null; // Returns null if the date string is invalid.
+        }
     }
-    }
+}
     
 
--- EventManager/src/Location.java
+++ EventManager/src/Location.java
@@ -1,19 +1,21 @@
 public enum Location{
     // Enum values
-    HLL114("Hill Center", "Busch"),
-    ARC103("Allison Road Classroom", "Busch"),
-    BE_AUD("Beck Hall", "Livingston"),
-    TIL232("Tillett Hall", "Livingston"),
-    AB2225("Academic Building", "College Avenue"),
-    MU302("Murray Hall", "College Avenue");
+    HLL114("HILL114","Hill Center", "Busch"),
+    ARC103("ARC103","Allison Road Classroom", "Busch"),
+    BE_AUD("BE_AUD","Beck Hall", "Livingston"),
+    TIL232("TIL232","Tillett Hall", "Livingston"),
+    AB2225("AB2225","Academic Building", "College Avenue"),
+    MU302("MU302","Murray Hall", "College Avenue");
 
     // Enum fields
     private final String building;
     private final String Campus;
+    private final String Room;
 
     Location(String Room, String building, String Campus) {
         this.building = building;
         this.Campus = Campus;
+        this.Room = Room;
     }
     // Getter methods for fields (if needed)
     public String getBuilding() {
@@ -22,4 +24,8 @@ public enum Location{
     public String getCampus() {
         return this.Campus;
     }
+    public String getRoom(){
+        return this.Room;
+    }
+
 }
