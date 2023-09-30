7834f88f47e339743e6c6c5fb81618281b236856
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-09-30 17:08:06 -0400

    Update Location.java
    added descriptions

--- EventManager/src/Location.java
+++ EventManager/src/Location.java
@@ -1,3 +1,10 @@
+/**
+ * The Location enum defines constants representing various locations, each with a room, a building, and a campus. 
+ * It provides methods to get the building, campus, and room of a location and to compare locations by campus.
+ *
+ * @author Digvijay Singh, Arun Felix
+ */
+
 public enum Location{
     HLL114("HILL114", "Hill Center", "Busch"),
     ARC103("ARC103", "Allison Road Classroom", "Busch"),
@@ -10,12 +17,20 @@ public enum Location{
     private final String campus;
     private final String room;
 
+     /**
+     * Constructs a Location constant with the given room, building, and campus.
+     *
+     * @param room     The room of the location.
+     * @param building The building of the location.
+     * @param campus   The campus of the location.
+     */
     Location(String room, String building, String campus) {
         this.building = building;
         this.campus = campus;
         this.room = room;
     }
 
+    //Getters for all instance variables 
     public String getBuilding() {
         return this.building;
     }
@@ -27,6 +42,13 @@ public enum Location{
     public String getRoom() {
         return this.room;
     }
+
+     /**
+     * Compares the campus of this location with the campus of another location lexicographically.
+     * 
+     * @param o The location to be compared.
+     * @return A negative integer, zero, or a positive integer as this campus is less than, equal to, or greater than the specified campus.
+     */
     public int comparebyCampus(Location o){
         return this.campus.compareTo(o.campus);
     }
